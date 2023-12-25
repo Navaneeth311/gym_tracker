4d72150fd5378106613ad76498da12b77816b4b5
Navaneeth <navaneeth@happyfox.com>
2023-12-25 06:12:54 +0530

    Request and Response usage

--- workout_tracker/set_tracker/urls.py
+++ workout_tracker/set_tracker/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import tracker_list, tracker_details
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("trackers/", tracker_list),
     path("tracker/<int:pk>/", tracker_details),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
