b129b5d9f460b810f2e27f7ab217b996808fce11
Navaneeth <navaneeth311@gmail.com>
2024-12-15 09:02:36 +0530

    Update urls.py

--- workout_tracker/set_tracker/urls.py
+++ workout_tracker/set_tracker/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("tracker/<int:pk>/", views.TrackerDetails.as_view()),
     path("users/", views.UserList.as_view(), name="users-list"),
     path("user/<int:pk>/", views.UserDetails.as_view()),
-    
+    path("user/<int:pk>/action", views.UserDetails.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
